6fe53a699784556505a6a2595c70885ada5f29dc
Ian Musamali <ian.musamali12@gmail.com>
2025-10-29 14:10:18 -0400

    Create structuralFA_Testbench.v

---
+++ structuralFA_Testbench.v
@@ -0,0 +1,49 @@
+// Testbench code of the structural code for full adder 
+`timescale 10ns/ 10ps;
+module Testbench_structural_adder();
+ reg A,B,Cin;
+ wire S,Cout;  
+ //Verilog code for the structural full adder 
+ Full_Adder_Structural_Verilog structural_adder(
+    .X1(A),
+    .X2(B),
+    .Cin(Cin),
+    .S(S),
+    .Cout(Cout) 
+   );
+ initial begin
+   A = 0;
+   B = 0;
+   Cin = 0;
+   #10;
+   A = 0;
+   B = 0;
+   Cin = 1;
+   #10;  
+   A = 0;
+   B = 1;
+   Cin = 0;
+   #10;
+   A = 0;
+   B = 1;
+   Cin = 1;
+   #10;
+   A = 1;
+   B = 0;
+   Cin = 0;
+   #10;
+   A = 1;
+   B = 0;
+   Cin = 1;
+   #10;
+   A = 1;
+   B = 1;
+   Cin = 0;
+   #10;  
+   A = 1;
+   B = 1;
+   Cin = 1;
+   #10;  
+  end
+      
+endmodule 
